475af47123e3a593f204ee78ac527fd2ca1760c0
sinhu-jung <tlsgn26@naver.com>
2021-06-25 15:08:39 +0900

    javascript 연습#1

---
+++ javascript-practices/ex02.js
@@ -0,0 +1,27 @@
+/*
+변수와 데이터 타입
+
+[기본타입(primitive type)]
+1. number
+2. boolean
+3. string
+4. undefined
+
+[객체]
+Number()
+Boolean()
+String()
+
+*/
+
+// 자바 스크립트는 선언과 정의의 구분이 없음 
+// var = 변수 이름을 정의하는 키워드
+var u; // var u = undefined; 와 동일
+var i = 10;
+var b = true;
+var s = "Hello World";
+
+console.log("u: " + typeof(u));
+console.log("i: " + typeof(i));
+console.log("b: " + typeof(b));
+console.log("s: " + typeof(s));
