fb204724da41651f24363df6eb037e2798652f34
sinhu-jung <tlsgn26@naver.com>
2021-06-30 10:52:23 +0900

    call, apply, bind #1

--- ch05/src/main/webapp/tabbox.js
+++ ch05/src/main/webapp/tabbox.js
@@ -1,12 +1,14 @@
-// DOM Level 2 Event 처리: element의 event에 Listener를 등록하는 방식
-// 2. JavaScript code(DOM API)로만 작성하기
-
 var tabBox = {
-	onTabClicked: function(){},
-	init: function(){
-		liTabs[i].addEventListener("click", this.onTabClicked);
-	},
-};
+	init: function() {
+		var divTabBox = document.getElementsByClassName("tab-box")[0];
+		var ul = divTabBox.childNodes[1];
+		var liTabs = ul.getElementsByTagName("li");
+		
+		for(var i = 0; i < liTabs.length; i++){
+			liTabs[i].addEventListener("click", onTabClicked);
+		}
+	}
+}
 
 var onTabClicked = function() {
 	//unselected
@@ -16,13 +18,3 @@ var onTabClicked = function() {
 	// selected
 	this.className = "selected";
 };
-
-window.onload = function() {
-	var divTabBox = document.getElementsByClassName("tab-box")[0];
-	var ul = divTabBox.childNodes[1];
-	var liTabs = ul.getElementsByTagName("li");
-	
-	for(var i = 0; i < liTabs.length; i++){
-		liTabs[i].addEventListener("click", onTabClicked);
-	}
-}
