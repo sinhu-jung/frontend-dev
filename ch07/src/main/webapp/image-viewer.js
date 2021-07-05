ac4130db5b819e8b2d678b68b51fbf1a8620ff76
sinhu-jung <tlsgn26@naver.com>
2021-07-05 15:07:25 +0900

    imageViewer #3

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,24 +1,78 @@
 var imageViewer = {
 	init: function() {
-		var _this = this;
+		var that = this;
 		$(function(){
-			$("#btn-change").click(_this._changeImage.bind(_this));
+			$("#btn-change").click(that._changeImage.bind(that));
+			$("#btn-sildeshow").click(that._slideShow.bind(that));
+			$(".image-viewer img").dblclick(that._consoleLog);
+			
+			// 첫 번째 이미지 선택
+			that._changeImage();
 		});
 	},
-	_changeImage: function() {
-		var result = Math.floor( Math.random() * this._images.length);
-		var data = this._images[result];
-		console.log(data);
+	
+	_consoleLog: function(){
+		var name = $(this).attr("alt");
+		console.log(name);
 	},
 	
-	_images: [ 
-		"국화:Chrysanthemum.jpg", 
-		"사막:Desert.jpg", 
-		"수국:Hydrangeas.jpg", 
-		"해파리:Jellyfish.jpg", 
-		"코알라:Koala.jpg", 
-		"등대:Lighthouse.jpg", 
-		"펭귄:Penguins.jpg", 
-		"툴립:Tulips.jpg"
+	_changeImage: function() {
+		var index = Math.floor( Math.random() * this._images.length);
+		var info = this._images[index];
+		$(".image-viewer img").attr({
+			src: "images/" + info.file,
+			alt : info.name,
+			title: info.name
+			});
+	},
+	
+	_intervalId: null,
+	_slideShow: function(){
+		if(this._intervalId){
+			/* 슬라이드쇼가 진행 상태, 버튼텍스트가 슬라이드쇼 중지 */
+			
+			// 1. 타이머 중지
+			clearInterval(this._intervalId);
+			this._intervalId = null;
+			
+			// 2. 버튼텍스트를 => 슬라이드쇼 시작
+			$("#btn-sildeshow").text("슬라이드쇼 시작");
+		} else {
+			/* 슬라이드쇼가 중지 상태, 버튼텍스트가 슬라이드쇼 시작  */
+			
+			// 1. 타이머를 시작
+			var that = this;
+			that._intervalId = setInterval(that._changeImage.bind(that), 1000);
+			// 2. 버튼텍스트를 => 슬라이드쇼 중지
+			$("#btn-sildeshow").text("슬라이드쇼 중지");
+		}
+	},
+	
+	_images: [
+		 {
+			name : "국화",
+			file : "Chrysanthemum.jpg"
+		 },{ 
+		 	name : "사막",
+		 	file : "Desert.jpg"
+		 },{
+		 	name: "수국",
+		 	file: "Hydrangeas.jpg"
+		 },{
+		 	name : "해파리",
+		 	file: "Jellyfish.jpg"
+		 },{ 
+			name :"코알라",
+			file : "Koala.jpg"
+		 },{
+		 	name: "등대",
+		 	file: "Lighthouse.jpg"
+		 },{ 
+		 	name: "펭귄",
+		 	file: "Penguins.jpg"
+		 },{ 
+		 	name:"툴립" ,
+		 	file:"Tulips.jpg"
+		 }
 		]
 }
