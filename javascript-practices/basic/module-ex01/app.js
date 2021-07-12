c363e90262c5b73f5ef9cb2cb26f091763d10c65
sinhu-jung <tlsgn26@naver.com>
2021-07-12 10:50:04 +0900

    ex6 이전 모듈 시스템 연습

---
+++ javascript-practices/basic/module-ex01/app.js
@@ -0,0 +1,8 @@
+/* app moule */
+const App = function(){
+    const app = {};
+    app.textContent = "Hello World";
+    return app;
+}
+
+module.exports = App;
---
+++ javascript-practices/basic/module-ex02/index.js
@@ -0,0 +1,4 @@
+const math = require('./math.js');
+console.log(math.PI);
+console.log(math.min(-3, -2, -1, 0, 1, 2, 3));
+console.log(math.max(-3, -2, -1, 0, 1, 2, 3));
